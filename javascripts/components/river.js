2c09572b395d9a758a7fe36226c82f57233f1193
Liza Star <lizadstar18@gmail.com>
2020-03-02 10:21:45 -0600

    bears are priting!

--- javascripts/components/river.js
+++ javascripts/components/river.js
@@ -1,16 +1,17 @@
 import util from '../helpers/util.js';
 import bearsData from '../helpers/data/bearsData.js'
 const printAllBears = () => {
-    let domString = ''
     const allBears = bearsData.getBears();
-    allBears.forEach((x)=> {
-    domString += 'div class="card" style="width: 18rem;">'
-    domString += `<img src="${x.imageUrl}" class="card-img-top" alt="...">`
+    let domString = ''
+    allBears.forEach((bear)=> {
+    domString += '<div class="card">'
+    domString += `<img src="${bear.imageUrl}" class="card-img-top" alt="...">`
     domString += '<div class="card-body">'
-    domString += `<h5 class="card-title">${x.name}</h5>`
+    domString += `<h5 class="card-title">${bear.name}</h5>`
     domString += '</div>' 
     domString += '</div>'
-    })
-
+   
+    });
+    util.printToDom('river', domString);
 };
 export default { printAllBears }
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,8 @@
 import bears from './components/bears.js'
+import printBears from './components/river.js'
+
 const init = () => {
     bears.sumbitBearEvent();
+    printBears.printAllBears();
 }
 init();
